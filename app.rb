957d4a1e1c315cb71fc7f8166b0bdbab7b2ad5ef
zzj <dh@MacBook-Pro.local>
2019-02-09 23:55:12 -0800

    app.rb fix

--- app.rb
+++ app.rb
@@ -39,7 +39,19 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess(letter)
+    begin
+      if letter == "" || letter == nil
+        raise ArgumentError
+      elsif @game.guesses.include?(letter) || @game.wrong_guesses.include?(letter)
+        flash[:message] = "You have already used that letter."
+        redirect '/show'
+      else
+        @game.guess(letter)
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+      redirect '/show'
+    end
     redirect '/show'
   end
   
