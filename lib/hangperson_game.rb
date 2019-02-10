b82436eb08dec14674314e4b64d6b52023de3508
zzj <dh@MacBook-Pro.local>
2019-02-09 21:28:24 -0800

    complete part 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,5 @@
 class HangpersonGame
-
+  attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -10,6 +10,8 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -23,5 +25,51 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
+  
+  def new(word)
+    initialize(word)
+  end
 
+  def guess(letter)
+    if letter == nil
+      raise ArgumentError
+    end
+    letter = letter.downcase
+    is_letter = letter =~ /[A-Za-z]/
+    if !is_letter
+      raise ArgumentError
+    end
+    if !(@guesses.include?(letter)) && !(@wrong_guesses.include?(letter))
+      if @word.include?(letter)
+        @guesses += letter
+      else
+        @wrong_guesses += letter
+      end
+      return
+    end
+    return false
+  end
+
+  def word_with_guesses
+    to_display = ""
+    @word.split("").each do |i|
+      if @guesses.include?(i)
+        to_display += i
+      else
+        to_display += "-"
+      end
+    end
+    return to_display
+  end
+
+  def check_win_or_lose
+    change_rem = 7 - (@guesses.length + @wrong_guesses.length)
+    if (@guesses.length == @word.length) && (change_rem > 0)
+      return :win
+    elsif change_rem > 0
+      return :play
+    else
+      return :lose
+    end
+  end
 end
